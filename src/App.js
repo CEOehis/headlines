37391420887da5cd0491ce3c97385c9796be4bb6
ceoehis <ceo.ehis@outlook.com>
2018-04-03 15:51:38 +0100

    add news item display card

--- src/App.js
+++ src/App.js
@@ -1,17 +1,56 @@
 import React, { Component } from 'react';
+import { Card, CardImg, CardText, CardBody,
+  CardTitle, Button } from 'reactstrap';
 import './App.css';
 
+const PATH_BASE = 'https://newsapi.org/v2/';
+const PARAM_TOP = 'top-headlines?';
+const PATH_API = 'apiKey=d371786357714d1db3a78f9fda001e81';
 
 class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      articles: []
+    }
+  }
+
+  componentDidMount() {
+    const queryUrl = `${PATH_BASE}${PARAM_TOP}country=us&${PATH_API}`;
+    console.log(queryUrl);
+    fetch(queryUrl)
+      .then(res => res.json())
+      .then(res => {
+        console.log(res.articles);
+        this.setState({
+          articles: res.articles
+        });
+      });
+  }
+
   render() {
+    const { articles } = this.state;
     return (
-      <div className="App">
-        <header className="App-header">
-          <h1 className="App-title">Welcome to React</h1>
+      <div className="">
+        <header className="">
+          <h1 className="text-center">Headlines</h1>
         </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <main className="container-fluid">
+          <div className="row">
+            {articles.map(article =>
+              <div key={article.url} style={{marginBottom: '20px'}} className="col-md-4">
+                <Card>
+                  <CardImg style={{height: '265px' }} top width="100%" src={article.urlToImage} />
+                  <CardBody>
+                    <CardTitle>{article.title}</CardTitle>
+                    <CardText>{article.description}</CardText>
+                    <Button>Button</Button>
+                  </CardBody>
+                </Card>
+              </div>
+            )}
+          </div>
+        </main>
       </div>
     );
   }
