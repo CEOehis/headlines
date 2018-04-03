3d94be1c03628365e1fe25f9a38c25a98a879edd
ceoehis <ceo.ehis@outlook.com>
2018-04-03 17:03:08 +0100

    add country select functionality

--- src/App.js
+++ src/App.js
@@ -12,12 +12,20 @@ class App extends Component {
   constructor() {
     super();
     this.state = {
-      articles: []
+      articles: [],
+      country: 'us'
     }
+
+    this.handleCountryChange = this.handleCountryChange.bind(this);
+    this.fetchCountryStories = this.fetchCountryStories.bind(this);
   }
 
   componentDidMount() {
-    const queryUrl = `${PATH_BASE}${PARAM_TOP}country=us&${PATH_API}`;
+    this.fetchCountryStories(this.state.country)
+  }
+  
+  fetchCountryStories(countryCode) {
+    const queryUrl = `${PATH_BASE}${PARAM_TOP}country=${countryCode}&${PATH_API}`;
     console.log(queryUrl);
     fetch(queryUrl)
       .then(res => res.json())
@@ -26,20 +34,28 @@ class App extends Component {
         this.setState({
           articles: res.articles
         });
-      });
+      });    
+  }
+
+  handleCountryChange(event) {
+    console.log(event.target.value);
+    this.setState({
+      country: event.target.value
+    });
+    this.fetchCountryStories(event.target.value);
   }
 
   render() {
-    const { articles } = this.state;
+    const { articles, country } = this.state;
     return (
       <div className="">
         <header className="">
           <h1 className="text-center">Headlines</h1>
         </header>
         <nav>
-          <select>
+          <select value={country} onChange={this.handleCountryChange}>
           {countries.map(country =>
-            <option key={country.code} value={country.code}>{country.name}</option>
+            <option key={country.code} value={country.code.toLowerCase()}>{country.name}</option>
           )}
           </select>
         </nav>
